e8e4f33dfe3772a06ef3c59ee49216b9a7baf9e2
Michael Edes <michaeledes.dev@gmail.com>
2024-11-29 22:09:22 +0000

    basic setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <h1>AI AGENT SCRIPT BUILD</h1>
+    </main>
+  );
+}
